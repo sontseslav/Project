6f762d1dbbabd41818aca17297313f50c6ae1dc7
sontseslav <s.khvalinsky@gmail.com>
2015-07-31 22:34:59 +0300

    7 Stage

---
+++ src/view/JATanks.java
@@ -0,0 +1,18 @@
+package view;/**
+ * Created by coder on 25.07.15.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JATanks extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
